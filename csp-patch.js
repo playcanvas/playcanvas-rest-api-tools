9436a7179eaa1407f7dde95b9ff3f6852bc949a4
Steven Yau <syau@snapchat.com>
2021-09-10 14:30:05 +0100

    Added CSP rules to index.html in preload bundles

--- csp-patch.js
+++ csp-patch.js
@@ -9,6 +9,28 @@ const shared = require('./shared');
 const config = shared.readConfig();
 
 
+function updatePreloadBundles(rootFolder) {
+    return new Promise((resolve) => {
+        (async function() {
+            // Check if the preload bundles exist
+            const updateBundle = async function(bundleName) {
+                if (fs.existsSync(bundleName)) {
+                    console.log("↪️ Updating " + path.basename(bundleName));
+
+                    const tempFolder = await shared.unzipProject(bundleName, 'bundle-temp');
+                    await addCspMetadata(tempFolder);
+                    await shared.zipProject(tempFolder, bundleName);
+                }
+            }
+
+            await updateBundle(rootFolder + '/preload-android.zip');
+            await updateBundle(rootFolder + '/preload-ios.zip');
+
+            resolve(rootFolder);
+        })();
+    });
+}
+
 function addCspMetadata(projectLocation) {
     return new Promise((resolve, reject) => {
         console.log("✔️ Adding CSP");
@@ -41,9 +63,14 @@ function getCspMetadataTag() {
     return tag.replace("{0}", content);
 }
 
+// Unzip the build and change the index.html CSP
+// If there are preload bundles, then the index.html in the
+// bundles will need to be modified too
+
 shared.downloadProject(config, "temp/downloads")
     .then((zipLocation) => shared.unzipProject(zipLocation, "contents/"))
     .then(addCspMetadata)
+    .then(updatePreloadBundles)
     .then((rootFolder) => shared.zipProject(rootFolder, 'temp/out/'+config.playcanvas.name+'_WithCSP.zip'))
     .then(outputZip => console.log("Success", outputZip))
     .catch(err => console.log("Error", err));
