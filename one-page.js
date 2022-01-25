f9ab2cd7af1d535d7a7b5d6154919cf0f7699ab8
Steven Yau <syau@snapchat.com>
2022-01-25 12:59:28 +0000

    Catered for preload bundles to convert png and jpg to WebP

--- one-page.js
+++ one-page.js
@@ -11,18 +11,14 @@ var externFiles = [
 ];
 
 
-function inlineAssets(projectPath) {
+function convertAssetsToWebP(projectPath) {
     return new Promise((resolve, reject) => {
         (async function() {
-            // 4. Open config.json and replace urls with base64 strings of the files with the correct mime type
-            // 5. In config.json, remove hashes of all files that have an external URL
             await (async function() {
-                console.log("↪️ Base64 encode all urls in config.json");
+                console.log("↪️ Converting to WebP");
 
                 var location = path.resolve(projectPath, "config.json");
                 var contents = fs.readFileSync(location, 'utf-8');
-
-                // Get the assets and Base64 all the files
 
                 var configJson = JSON.parse(contents);
                 var assets = configJson.assets;
@@ -41,85 +37,37 @@ function inlineAssets(projectPath) {
                     var urlSplit = url.split('.');
                     var extension = urlSplit[urlSplit.length - 1];
 
+                    var fileExists = true;
+
                     var filepath = path.resolve(projectPath, url);
                     if (!fs.existsSync(filepath)) {
                         console.log("   Cannot find file " + filepath + " If it's a loading screen script, please ignore");
-                        continue;
+                        fileExists = false;
                     }
 
                     if (extension === 'png') {
-                        console.log(filepath);
-                        // await imagemin([filepath], {
-                        //     destination: path.dirname(filepath),
-                        //     plugins: [
-                        //         imageminWebp(
-                        //             {
-                        //                 lossless: true,
-                        //                 alphaQuality: 100,
-                        //                 quality: 100
-                        //             }
-                        //         )
-                        //     ]
-                        // });
+                        console.log('   ' + filepath);
 
-                        spawnSync('cwebp', ['-lossless', '-q', '100', '-alpha_q', '100', '-exact', filepath, '-o', filepath.replace('png', 'webp')]);
-                        fs.unlinkSync(filepath);
+                        if (fileExists) {
+                            spawnSync('cwebp', ['-lossless', '-q', '100', '-alpha_q', '100', '-exact', filepath, '-o', filepath.replace('png', 'webp')]);
+                            fs.unlinkSync(filepath);
+                        }
 
                         asset.file.url = url.replace('.png', '.webp');
-                        console.log(asset.file.url);
+                        console.log('   ' + asset.file.url);
                     }
 
                     if (extension === 'jpeg' || extension === 'jpg') {
-                        console.log(filepath);
-                        // await imagemin([filepath], {
-                        //     destination: path.dirname(filepath),
-                        //     plugins: [
-                        //         imageminWebp(
-                        //             {
-                        //                 lossless: false,
-                        //                 alphaQuality: 100,
-                        //                 quality: 80
-                        //             }
-                        //         )
-                        //     ]
-                        // });
+                        console.log('   ' + filepath);
 
-                        spawnSync('cwebp', ['-q', '80', filepath, '-o', filepath.replace('jpeg', 'webp').replace('jpg', 'webp')]);
-
-                        fs.unlinkSync(filepath);
+                        if (fileExists) {
+                            spawnSync('cwebp', ['-q', '80', filepath, '-o', filepath.replace('jpeg', 'webp').replace('jpg', 'webp')]);
+                            fs.unlinkSync(filepath);
+                        }
 
                         asset.file.url = url.replace('.jpeg', '.webp').replace('.jpg', '.webp');
-                        console.log(asset.file.url);
+                        console.log('   ' + asset.file.url);
                     }
-
-                    // var fileContents;
-
-                    // var mimeprefix = "data:application/octet-stream";
-                    // switch(extension) {
-                    //     case "png":
-                    //         mimeprefix = "data:image/png";
-                    //     break;
-
-                    //     case "jpeg":
-                    //     case "jpg":
-                    //         mimeprefix = "data:image/jpeg";
-                    //     break;
-                    // }
-
-                    // var b64;
-
-                    // if (isText) {
-                    //     b64 = btoa(unescape(encodeURIComponent(fileContents)));
-                    // } else {
-                    //     var ba = Uint8Array.from(fileContents);
-                    //     b64 = base64js.fromByteArray(ba);
-                    // }
-
-                    // // As we are using an escaped URL, we will search using the original URL
-                    // asset.file.url = mimeprefix + ';base64,' + b64;
-
-                    // Remove the hash to prevent appending to the URL
-                    asset.file.hash = "";
                 };
 
                 fs.writeFileSync(location, JSON.stringify(configJson));
@@ -129,11 +77,37 @@ function inlineAssets(projectPath) {
     });
 }
 
+
+function updatePreloadBundles(rootFolder) {
+    return new Promise((resolve) => {
+        (async function() {
+            // Check if the preload bundles exist
+            const updateBundle = async function(bundleName) {
+                if (fs.existsSync(bundleName)) {
+                    console.log("↪️ Updating " + path.basename(bundleName));
+
+                    const tempFolder = await shared.unzipProject(bundleName, 'bundle-temp');
+                    await convertAssetsToWebP(tempFolder);
+                    await shared.zipProject(tempFolder, bundleName);
+                }
+            }
+
+            await updateBundle(rootFolder + '/preload-android.zip');
+            await updateBundle(rootFolder + '/preload-ios.zip');
+
+            resolve(rootFolder);
+        })();
+    });
+}
+
+
+
 // Force not to concatenate scripts as they need to be inlined
-const zipLocation = "temp/downloads/FunRun_1035577_env.zip"
+const zipLocation = "temp/downloads/Bitmoji Plaza_WithCSP.zip"
 //const zipLocation = "temp/downloads/WebP-test.zip"
 shared.unzipProject(zipLocation, 'contents')
-    .then(inlineAssets)
-    .then((projectPath) => shared.zipProject(projectPath, 'temp/out/FunRun_1035577_env.zip'))
+    .then(convertAssetsToWebP)
+    .then(updatePreloadBundles)
+    .then((projectPath) => shared.zipProject(projectPath, 'temp/out/Bitmoji Plaza_WithCSP WebP.zip'))
     .then(outputHtml => console.log("Success", outputHtml))
     .catch(err => console.log("Error", err));
